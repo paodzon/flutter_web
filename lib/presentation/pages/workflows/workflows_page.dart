9075703c874ad56f3d0f5fa45380c7b045d0f339
paodzon <paolodizon@ubx.ph>
2023-03-22 18:23:29 +0800

    feature: added submodule

--- lib/presentation/pages/workflows/workflows_page.dart
+++ lib/presentation/pages/workflows/workflows_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web/app/config/app_colors.dart';
-import 'package:flutter_web/presentation/pages/workflows/views/number_pagination.dart';
+import 'package:flutter_web/presentation/pages/workflows/data.dart';
+
 import 'package:flutter_web/presentation/widgets/layout.dart';
+import 'package:textinput_builder/textinput_builder.dart';
 
 class WorkflowsPage extends StatefulWidget {
   const WorkflowsPage({Key? key}) : super(key: key);
@@ -18,17 +19,7 @@ class _WorkflowsPageState extends State<WorkflowsPage> {
       contentPage: Container(
         margin: const EdgeInsets.all(50),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          NumberPaginator(
-            numberPages: 50,
-            buttonShape: BeveledRectangleBorder(
-              borderRadius: BorderRadius.circular(2),
-            ),
-            buttonUnselectedForegroundColor: AppColors.gray700,
-            buttonSelectedBackgroundColor: const Color(0xffF7921E),
-            onPageChange: (int index) {
-              // handle page change...
-            },
-          )
+          FormBuilder(data: sampleJson, onSubmit: (val) => print(val))
         ]),
       ),
     );
